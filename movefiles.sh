a687324d3c5f7dde71830f7c775ba12cbd170489
swayampeandey <swayam265@gmail.com>
2022-06-10 00:04:59 +0530

    this is a demo file

---
+++ movefiles.sh
@@ -0,0 +1,15 @@
+for files in *.txt
+do
+        folderName=`echo $files | awk -F. '{print $1}'`;
+        echo $files;
+        echo $folderName;
+        printf "\n";
+        if [ -d $folderName ]
+        then
+                rm -r $folderName;
+        fi
+        mkdir $folderName;
+        mv $files $folderName/$files;
+done
+
+
